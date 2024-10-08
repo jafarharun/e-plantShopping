9aa6eda69b816a271d903f7052271e3399a61519
jafarharun <jafarharun7@gmail.com>
2024-10-08 13:41:54 +0100

    update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -262,6 +262,12 @@ const handlePlantsClick = (e) => {
     setShowCart(false);
   };
 
+  const handleParadiseNurseyClick = (e) => {
+    e.preventDefault(); // Prevent the default anchor click behavior
+    setShowPlants(false); // Hide the Plants section
+    setShowCart(false);   // Optionally hide the Cart section if needed
+};
+
   //Add a function to calculate the total quantity of items in the cart:
 
   const calculateTotalQuantity = () => {
@@ -280,7 +286,7 @@ const handlePlantsClick = (e) => {
             <div className="tag">
                <div className="luxury">
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-               <a href="/" style={{textDecoration:'none'}}>
+               <a href="#"  onClick={(e) => handleParadiseNurseyClick(e)} style={{textDecoration:'none'}}>
                         <div>
                     <h3 style={{color:'white'}}>Paradise Nursery</h3>
                     <i style={{color:'white'}}>Where Green Meets Serenity</i>
